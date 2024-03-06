601a571902d1b9fe3384ad07944ea0fcf63a03dc
CamiloRdgz <crodriguezg006@gmail.com>
2024-03-05 21:55:15 -0500

    Entrega 2 Desarrollo Web
    Entrega 2 lista de Desarrollo Web

--- clase4.js
+++ clase4.js
@@ -1,71 +1,80 @@
-//Arrays
-let numbers = [10.34, 1, 1000, 350.456];
-const various = ['Camilo', true, 10, null, 3.0]
+//Creacion de los 4 Arrays
+let num
+let sum
+//Rellena el array con Mr. o Mrs. de forma aleatoria
+let profesores = []
+for (let i = 0; i <= 10; i++) {
+    num = Math.floor(Math.random() * 2 + 1)
+    if (num == 1) {
+        profesores.push("Mr. " + i)
+    } else if (num == 2) {
+        profesores.push("Mrs. " + i)
+    }
+}
 
-console.log(numbers);
-console.log(various[0]);
-console.log(various);
+//Rellena el array con Masculino o Femenino de forma aleatoria
+let genero = []
+for (let j = 0; j <= 10; j++) {
+    num = Math.floor(Math.random() * 2 + 1)
+    if (num == 1) {
+        genero.push("Masculino")
+    } else if (num == 2) {
+        genero.push("Femenino")
+    }
+}
 
-//We add a value at the end of the array
-numbers.push("Colombia");
-console.log(numbers);
+//Rellena el array con notas academicas aleatorias
+let notas = []
+for (let h = 0; h <= 10; h++) {
+    num = Math.random() * 5
+    num = num.toFixed(1) //Lo convertimos a que muestre solo un decimal
+    notas.push(num)
+}
 
-//We remove the value at the end of the array
-numbers.pop();
-console.log(numbers);
+//Array de paises
+let paises = ["Albania","Armenia","Algeria","Bahrein","Belgica","Belize","Cambodia","Comoros","Colombia","Cuba","Dinamarca","Ucrania","Zimbabwe","Zambia"]
 
-//We add a value at the beginning of the array
-numbers.unshift("Brasil");
-console.log(numbers);
-
-//We remove the value at the end of the array
-numbers.shift();
-console.log(numbers)
-
-//Array only methods
-
-//1 .reduce()
-let reduced = numbers.reduce((acumulator, actual_value) => {
-    return acumulator + actual_value;
-});
-console.log(reduced);
+console.log("Arrays creados")
+console.log(profesores)
+console.log(genero)
+console.log(notas)
+console.log(paises)
 
 
-//2. .filter()
-let filtered = numbers.filter(dato => {
-    return dato > 10;
+
+//Funciones por cada array
+console.log("Usando metodos de array")
+
+//Retorna la posicion solo si el numero aleatorio generado es par
+let random = genero.filter(puntero => {
+    if((Math.floor(Math.random() * 100) % 2) == 0) {
+        return puntero
+    }
 })
-console.log(filtered);
+console.log(random)
 
-let countries = ["Colombia", "Mexico", "Congo", "Comoros", "Seychelles", "Spain", "Venezuela", "Peru", "Bolivia"];
+//Devuelve los paises cuya longitud sean mayores o igual a 7 letras
+let longitudPais = paises.filter(longi => {
+    longi = longi.length
+    if (longi >= 7) {
+        return longi
+    }
+})
+console.log(longitudPais)
 
-console.log(countries.filter(dato => {return dato.startsWith("Co")}));
+//Devuelve el promedio de todas las notas
+let reducido = notas.reduce((acumulador, nota_actual) => {
+    acumulador = parseFloat(acumulador)
+    nota_actual = parseFloat(nota_actual)
+    
+    return acumulador += nota_actual
+});
+console.log((reducido / notas.length).toFixed(2));
 
-
-//3. .map()
-let mapping = countries.map(country => {return `Ejecuten a los de ${country}`});
-console.log(mapping);
-let mapping2 = numbers.map(squared => {return Math.pow(squared, 2)});
-console.log(mapping2);
-
-
-//4. .forEach()
-console.time("first");
-numbers.forEach(number => {console.log(number)});
-console.timeEnd("first");
-
-console.time("second");
-for(let i = 0; i > numbers.length; i++) {
-    console.log(numbers[i]);
-}
-console.timeEnd("second")
-
-console.time("third");
-for(let i of numbers){
-    console.log(numbers[i]);
-}
-console.timeEnd("third");
-
-
-//CREAR 4 ARREGLOS USANDO FUNCIONES (FLECHA)
-//USAR METODOS DE ARREGLOS Y MOSTRAR RESULTADOS (PARA CADA ARREGLO MINIMO UN METODO (SON CUATRO ARREGLOS))
+//Devuelve todos los profesores de genero masculino
+let generos = profesores.filter(generoProfe => {
+    if (generoProfe.startsWith("Mr.")) {
+        return generoProfe
+    }
+})
+console.log(generos)
